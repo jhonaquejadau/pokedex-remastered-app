fabeaefed2bfadd49adeb0f71ac8075af090bd65
jhonaquejadau <jhona.quejada@gmail.com>
2023-02-15 23:52:22 -0500

    Routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import { Home, Login } from '@src/pages'
-import './App.css'
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Footer, Navbar } from "./components";
+import { PublicRoutes } from "./models";
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const Login = lazy(() => import("./pages/Login/Login"));
+const Pokedex = lazy(() => import("./pages/Pokedex/Pokedex"));
 
 function App() {
-
   return (
     <main>
-      <h1>POKEDEX REMASTERED APP</h1>
-      <Home />
-      <Login />
+      <Suspense fallback={<>CARGANDO ALGO...</>}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path={PublicRoutes.LOGIN} element={<Login />} />
+          <Route path={PublicRoutes.POKEDEX} element={<Pokedex />} />
+        </Routes>
+        <Footer />
+      </Suspense>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
