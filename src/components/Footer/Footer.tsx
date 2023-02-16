52171a3b1cdf89be313b341c46c0cb80be93b9f5
jhonaquejadau <jhona.quejada@gmail.com>
2023-02-16 14:45:36 -0500

    components refactor

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <footer className='absolute bottom-0 left-0 right-0 bg-slate-300'>
+    <footer className='w-full bg-slate-300'>
       Footer copyrigth
     </footer>
   )
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,9 +1,9 @@
-import { CircularProgress } from "@mui/material";
+import pokeball from "../../assets/pokeball.svg"
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center w-full h-screen bg-orange-500">
-      <CircularProgress />
+    <div className="flex justify-center items-center w-full h-[100vh]">
+      <img src={pokeball} alt="pokeball" className="animate-spin w-40 h-40"/>
     </div>
   );
 };
