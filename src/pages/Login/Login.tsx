437d7f2f28022bd16fff296ad582db5905062446
jhonaquejadau <jhona.quejada@gmail.com>
2023-02-17 23:47:35 -0500

    Login first design and control inputs

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { SignIn, SignUp } from './components';
+import React from "react";
+import { SignIn, SignUp } from "./components";
 
 export interface LoginInterface {}
 
-const Login : React.FC<LoginInterface> = () => {
-	return <div>
-		<SignIn/>
-		<SignUp/>
-	</div>;
+const Login: React.FC<LoginInterface> = () => {
+  return (
+    <div className="w-full h-[90vh]">
+      <SignIn />
+      {/* <SignUp /> */}
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Login/components/SignIn/SignIn.tsx
+++ src/pages/Login/components/SignIn/SignIn.tsx
@@ -1,8 +1,86 @@
-import React from 'react';
-export interface SignInInterface {}
+import { TextField } from "@mui/material";
+import { useState } from "react";
+import background from "../../../../assets/login.svg";
 
-const SignIn : React.FC<SignInInterface> = () => {
-	return <div>SignIn</div>;
+interface FormDataInterface {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const SignIn = () => {
+  const [formData, setFormData] = useState<FormDataInterface>({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleOnChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: any, data: FormDataInterface) => {
+    event.preventDefault();
+    console.log(data);
+  };
+
+  return (
+    <div className="flex flex-row w-full h-full text-center">
+      <div className="flex flex-col justify-center w-[50%] h-full shadow-xl">
+        <h1 className="text-4xl">WELCOME BACK</h1>
+        <p>Login into your acocunt and manage your dahsboard...</p>
+        <form className="flex flex-col items-center gap-4 p-4">
+          <TextField
+            className="w-full"
+            label="name"
+            variant="outlined"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleOnChange}
+          />
+          <TextField
+            className="w-full"
+            label="email"
+            variant="outlined"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleOnChange}
+          />
+          <TextField
+            className="w-full"
+            label="password"
+            variant="outlined"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleOnChange}
+          />
+          <TextField
+            className="w-full"
+            label="confirmPassword"
+            variant="outlined"
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleOnChange}
+          />
+          <button
+            type="submit"
+            onSubmit={(event) => handleSubmit(event, formData)}
+            className="px-6 bg-slate-500 rounded"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+	  <img src={background} alt="background" className="w-[50%]"/>
+    </div>
+  );
 };
 
 export default SignIn;
