64134079c30aa7e1d23db2af55c7a204543fb5c6
jhonaquejadau <jhona.quejada@gmail.com>
2023-02-18 14:22:06 -0500

    Added form data for login & register

---
+++ src/utilities/login.data.ts
@@ -0,0 +1,10 @@
+interface LoginInterface {
+  id: string;
+  name: string;
+  type: string;
+}
+
+export const loginFields: LoginInterface[] = [
+  { id: "1", name: "email", type: "text" },
+  { id: "2", name: "password", type: "password" },
+];
