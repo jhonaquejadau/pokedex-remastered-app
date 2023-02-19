f1a444b0bfcbdc3722f4cd0ee83526f18e97e59b
jhonaquejadau <jhona.quejada@gmail.com>
2023-02-19 17:50:54 -0500

    Added components to handle private and not found routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import { lazy, Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./App.css";
 import { Footer, Navbar } from "./components";
 import { Loader } from "./components/Loader";
-import { PublicRoutes } from "./models";
+import { PrivateRoutes, PublicRoutes } from "./models";
+import { ProtectedRoutes } from "./protector";
+import { RoutesWithNotFound } from "./utilities";
 
 const Home = lazy(() => import("./pages/Home/Home"));
 const Login = lazy(() => import("./pages/Login/Login"));
-const SignUp = lazy(() => import("./pages/Login/components/SignUp/SignUp"))
+const SignUp = lazy(() => import("./pages/Login/components/SignUp/SignUp"));
 const Pokedex = lazy(() => import("./pages/Pokedex/Pokedex"));
+const Private = lazy(() => import("./pages/Private/Private"));
+
 
 function App() {
   return (
     <main>
       <Suspense fallback={<Loader />}>
         <Navbar />
-        <Routes>
+        <RoutesWithNotFound>
           <Route path="/" element={<Home />} />
           <Route path={PublicRoutes.LOGIN} element={<Login />} />
           <Route path={PublicRoutes.REGISTER} element={<SignUp />} />
           <Route path={PublicRoutes.POKEDEX} element={<Pokedex />} />
-        </Routes>
+          <Route element={<ProtectedRoutes/>}>
+            <Route path={`${PrivateRoutes.PRIVATE}/*`} element={<Private />} />
+          </Route>
+        </RoutesWithNotFound>
         <Footer />
       </Suspense>
     </main>
