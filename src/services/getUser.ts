ebefabd867aa8cc43af247fcda669274595b9355
jhonaquejadau <jhona.quejada@gmail.com>
2023-02-19 17:52:46 -0500

    Refactor getUser service

--- src/services/getUser.ts
+++ src/services/getUser.ts
@@ -3,9 +3,10 @@ import { collection, getDocs } from "firebase/firestore";
 
 export const getUserInfo = async (user_credential: any) => {
   return await getDocs(collection(db, "users")).then((results) => {
+    
     const userInfo = results.docs
       .map((doc) => doc.data())
       .filter((doc) => doc.token === user_credential.user.uid);
-    return { name: userInfo[0].name, nickname: userInfo[0].nickname };
+    return {name: userInfo[0].name, token: userInfo[0].token, nickname: userInfo[0].nickname, email: userInfo[0].email}
   });
 };
