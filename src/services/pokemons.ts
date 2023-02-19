797ef1aa3b12537bae91b2d7771450780c2663f3
jhonaquejadau <jhona.quejada@gmail.com>
2023-02-19 18:20:53 -0500

    Code refactor

--- src/services/pokemons.ts
+++ src/services/pokemons.ts
@@ -10,15 +10,3 @@ export const getPokemonsData = async (id_document: string) => {
   );
 };
 
-const pokemons_part_one: any = await getPokemonsData(
-  "Wk3Q6XDrYVsvVa557HYG"
-);
-
-const pokemons_part_two: any = await getPokemonsData(
-  "r0Y5v56I0qMV1CKHoPs2"
-);
-
-export const all_pokemons = JSON.parse(pokemons_part_one.part_one).concat(
-  JSON.parse(pokemons_part_two.part_two)
-);
-
