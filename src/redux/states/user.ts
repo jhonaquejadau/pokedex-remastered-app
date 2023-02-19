f8bdc181f0099327e83ee54811f80043c365b6b1
jhonaquejadau <jhona.quejada@gmail.com>
2023-02-19 17:53:50 -0500

    Added localStorage to persist data in redux state

--- src/redux/states/user.ts
+++ src/redux/states/user.ts
@@ -1,20 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { UserInterface } from "@/models";
 
-const EmptyUserState: UserInterface = {
-  id: 0,
+export const EmptyUserState: UserInterface = {
   token: "",
   name: "",
+  nickname: "",
   email: "",
 };
 
+//Persist user data in localstorage
+const persistLocalStorageUser = (userInfo: UserInterface) => {
+  localStorage.setItem("user", JSON.stringify({ ...userInfo }));
+};
+
+const clearLocalStorageUser = () => {
+  localStorage.removeItem("user");
+};
+
+const localStorageUser = localStorage.getItem("user");
+
 export const userSlice = createSlice({
   name: "user",
-  initialState: EmptyUserState,
+  initialState: localStorageUser ? JSON.parse(localStorageUser as string) : EmptyUserState,
   reducers: {
-    createUser: (state, action) => action.payload,
-    updateUser: (state, action) => ({ ...state, ...action.payload }),
-    resetUser: () => EmptyUserState,
+    createUser: (state, action) => {
+      persistLocalStorageUser(action.payload)
+      return action.payload
+    },
+    updateUser: (state, action) => {
+      const result = { ...state, ...action.payload } 
+      persistLocalStorageUser(result)
+      return result
+    },
+    resetUser: () => {
+      clearLocalStorageUser()
+      return EmptyUserState
+    },
   },
 });
 
