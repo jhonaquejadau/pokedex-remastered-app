2437edf29684a99f63a2d9ffdb65fb35223b6ec0
jhonaquejadau <jhona.quejada@gmail.com>
2023-02-20 19:56:05 -0500

    code refactor

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,7 +2,7 @@ export * from './Home';
 export * from './Login/components/SignUp';
 export * from './Login/components';
 export * from './Login';
-export * from './Pokedex/components/PokemonCards';
+export * from './Pokedex/components/Pokemons';
 export * from './Pokedex/components/Search';
 export * from './Pokedex/components';
 export * from './Pokedex';
