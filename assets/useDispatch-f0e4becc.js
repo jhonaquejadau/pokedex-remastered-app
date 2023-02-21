1aca6753e74570d2256bf9ac0a7447e5fee89d23
jhonaquejadau <jhona.quejada@gmail.com>
2023-02-20 19:56:41 -0500

    Updates

---
+++ assets/useDispatch-f0e4becc.js
@@ -0,0 +1,1 @@
+import{O as e,Q as u,r as c}from"./index-c9f7fe44.js";function r(t=e){const o=t===e?u:()=>c.useContext(t);return function(){const{store:s}=o();return s}}const a=r();function i(t=e){const o=t===e?a:r(t);return function(){return o().dispatch}}const x=i();export{x as u};
